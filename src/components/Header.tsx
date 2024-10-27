fb5c5455685fcd59c4c149950bdb63d389432953
Isa Par <isabelle826@hotmail.com>
2024-10-27 21:12:58 +0100

    Header media queries

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
 import "../styles/header.css";
 import Logo from "../assets/logo-sm.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 import { useState } from "react";
 
 export default function Header() {
@@ -12,11 +15,31 @@ export default function Header() {
                 <img src={Logo} alt="Logo" />
             </NavLink>
             <div className={`menu ${isOpen ? "open" : ""}`}>
-                <NavLink className={`header-navlink ${isOpen ? "open" : ""}`} to={"/search"}>Search</NavLink>
-                <NavLink className={`header-navlink ${isOpen ? "open" : ""}`} to={"/plant-care"}>Plant care</NavLink>
-                <NavLink className={`header-navlink ${isOpen ? "open" : ""}`} to={"/my-plants"}>My plants</NavLink>
+                <NavLink
+                    className={`header-navlink ${isOpen ? "open" : ""}`}
+                    to={"/search"}
+                    onClick={() => setIsOpen(false)}
+                >
+                    Search
+                </NavLink>
+                <NavLink
+                    className={`header-navlink ${isOpen ? "open" : ""}`}
+                    to={"/plant-care"}
+                    onClick={() => setIsOpen(false)}
+                >
+                    Plant care
+                </NavLink>
+                <NavLink
+                    className={`header-navlink ${isOpen ? "open" : ""}`}
+                    to={"/my-plants"}
+                    onClick={() => setIsOpen(false)}
+                >
+                    My plants
+                </NavLink>
             </div>
-            <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>🟰</div>
+            <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+                <FontAwesomeIcon icon={faBars} size="lg" style={{ color: "#fcfcec" }} />
+            </div>
         </nav>
     );
 }
